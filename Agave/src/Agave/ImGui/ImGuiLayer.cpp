3f4b557809aad63bed53cd4e3c6a3200bb4e7b8d
Joshua Palmer <joshua.after@gmail.com>
2022-07-18 11:02:24 -0600

    Comments

--- Agave/src/Agave/ImGui/ImGuiLayer.cpp
+++ Agave/src/Agave/ImGui/ImGuiLayer.cpp
@@ -17,22 +17,27 @@
 #include "Agave/Events/Event.h"
 #include "Agave/Core/Log.h"
 
+// Temporary
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
-
-// Temporary
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
 namespace Agave {
 
+    ///=========================================================================
+    ///=========================================================================
     ImGuiLayer::ImGuiLayer()
         : Layer("ImGuiLayer")
     {}
 
+    ///=========================================================================
+    ///=========================================================================
     ImGuiLayer::~ImGuiLayer()
     {}
 
+    ///=========================================================================
+    ///=========================================================================
     void ImGuiLayer::OnAttach() 
     {
         // Setup Dear ImGui context
@@ -66,6 +71,8 @@ namespace Agave {
         ImGui_ImplOpenGL3_Init();
     }
 
+    ///=========================================================================
+    ///=========================================================================
     void ImGuiLayer::OnDetach()
     {
         ImGui_ImplOpenGL3_Shutdown();
@@ -73,12 +80,16 @@ namespace Agave {
         ImGui::DestroyContext();
     }
 
+    ///=========================================================================
+    ///=========================================================================
     void ImGuiLayer::OnImGuiRender()
     {
         static bool show = true;
         ImGui::ShowDemoWindow(&show);
     }
 
+    ///=========================================================================
+    ///=========================================================================
     void ImGuiLayer::Begin()
     {
         ImGui_ImplOpenGL3_NewFrame();
@@ -86,6 +97,8 @@ namespace Agave {
         ImGui::NewFrame();
     }
 
+    ///=========================================================================
+    ///=========================================================================
     void ImGuiLayer::End()
     {
         ImGuiIO& io = ImGui::GetIO();
